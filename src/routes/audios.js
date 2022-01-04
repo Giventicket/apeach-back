3060b2675547884cc947d12d2d6aa003e40b6902
root <root@apeach-back.c.pathfinder-101.internal>
2022-01-04 06:41:18 +0000

    [feat] audio remove

--- src/routes/audios.js
+++ src/routes/audios.js
@@ -2,21 +2,17 @@ const express = require('express');
 const path = require('path');
 const multer = require('multer');
 const multerGoogleStorage = require('multer-google-storage');
+const {Storage} = require('@google-cloud/storage');
 
 const router = express.Router();
-
-const storage = multer.diskStorage({
-  filename: (req, file, cb) => {
-    cb(null, Date.now().toString(16) + '-' + file.fieldname + '.mp3')
-  }
-});
+const storage = new Storage({ keyFilename: process.env.KEY_FILENAME });
 
 const upload = multer({
-  storage: multerGoogleStorage.storageEngine({
-      bucket: "apeach-bucket",
-      projectId: "pathfinder-101",
-      keyFilename: "pathfinder-101-6f8d719fc233.json"
-  })
+    storage: multerGoogleStorage.storageEngine({
+        bucket: process.env.BUCKET_NAME,
+        projectId: process.env.PROJECT_ID,
+        keyFilename: process.env.KEY_FILENAME
+    })
 });
 
 // audio 저장하기
@@ -30,6 +26,21 @@ router.post('/upload', upload.single('audio'), async (req, res, next) => {
         res.status(200).json({
             message: "upload success [upload audio on google bucket]",
             data: req.file
+        });
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+});
+ 
+// audio 삭제하기
+router.delete('/:filename', async (req, res, next) => {
+    try {
+        const result = await storage.bucket('apeach-bucket').file(req.params.filename).delete();
+        console.log(result);
+        res.status(200).json({
+            message: `remove file ${req.params.filename} from google bucket`,
+            data: { }
         });
     } catch (error) {
         console.log(error);
