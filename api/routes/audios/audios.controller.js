e528eda5d26a3486a698a1c9f1a131fb2cc5f2c5
root <root@apeach-back.c.pathfinder-101.internal>
2022-01-11 05:01:31 +0000

    [feat] preprocessing

--- api/routes/audios/audios.controller.js
+++ api/routes/audios/audios.controller.js
@@ -1,17 +1,47 @@
 const formidable = require('formidable');
 const form = formidable({ multiples: true });
+const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs');
 
 const parseForm = (req, res, next) => {
     form.parse(req, (err, fields, files) => {
         try {
             req.files = files;
-            console.log(files);
+            //console.log(files);
             next();
         } catch (err) {
             next(err);
         }
     });
 };
+
+const preprocessSync = (filepath) => {
+    return new Promise((resolve,reject)=>{
+        ffmpeg(filepath)
+            .format('wav')
+            .outputOptions([
+                `-ar 16000`,
+                `-ac 1`,
+                `-sample_fmt s16`])
+            .output(`${filepath}R`)
+            .on('end', () => {
+                resolve();
+            })
+            .on('error', (err) => {return reject(new Error(err));})
+            .run();        
+   });
+};
+
+
+const preprocess = async (req, res, next) => {
+    try {
+        //ffmpeg -i {input} -ar 16000 -ac 1 -sample_fmt s16 {output}        
+        await preprocessSync(req.files.audio.filepath).then().catch((err) => {throw err;});
+        next();
+    } catch (err) {
+        next(err);
+    }
+}
 
 const uploadFile = async (req, res, next) => {
     try {
@@ -22,6 +52,10 @@ const uploadFile = async (req, res, next) => {
               contentType: req.files.audio.mimetype,
             },
         });
+        fs.unlink(req.files.audio.filepath,(err)=>{ 
+            if(err)
+                req.logger.error(`status: ${(err.status || 500)}, message: ${err.message}`);
+        });
         res.status(200).json({
             message: "upload success [upload audio on google bucket]",
             data: result[0].metadata
@@ -30,6 +64,33 @@ const uploadFile = async (req, res, next) => {
         next(err);
     }
 } 
+
+const uploadFileAfterPreprocesssing = async (req, res, next) => {
+    try {
+        const gcStorage = req.gcStorage;
+        const result = await gcStorage.bucket('apeach-bucket').upload(`${req.files.audio.filepath}R`, {
+            destination: `${req.files.audio.newFilename}R`,
+            metadata: {
+              contentType: 'audio/wave',
+            },
+        });
+        fs.unlink(req.files.audio.filepath,(err)=>{ 
+            if(err)
+                req.logger.error(`status: ${(err.status || 500)}, message: ${err.message}`);
+        });
+        fs.unlink(`${req.files.audio.filepath}R`,(err)=>{ 
+            if(err)
+                req.logger.error(`status: ${(err.status || 500)}, message: ${err.message}`);
+        });
+        res.status(200).json({
+            message: "upload success [upload preprocessed audio on google bucket]",
+            data: result[0].metadata
+        });
+    } catch (err) {
+        next(err);
+    }
+}
+
 
 const deleteFile = async (req, res, next) => {
     try {
@@ -44,4 +105,4 @@ const deleteFile = async (req, res, next) => {
     }
 }
 
-module.exports = {uploadFile, deleteFile, parseForm};
+module.exports = {uploadFile, deleteFile, parseForm, preprocess, uploadFileAfterPreprocesssing};
--- api/routes/audios/index.js
+++ api/routes/audios/index.js
@@ -1,31 +1,11 @@
 const express = require('express');
 const controller = require('./audios.controller');
-
+                       
 const router = express.Router();
 
-const preprocess = async (req, res, next) => {
-    try {
-        //ffmpeg -i {input} -ar 16000 -ac 1 -sample_fmt s16 {output}
-        let process = new ffmpeg(req.files.audio.filepath);
-        process.then((audio) => {
-            ffmpeg('/path/to/file.avi').inputOptions([
-            '-option1',
-            '-option2 param2',
-            '-option3',
-            '-option4 param4'
-            ]);
-        }, function (err) {
-            console.log('Error: ' + err);
-        });
-        next();
-    } catch (err) {
-        console.log(err);
-        next(err);
-    }
-}
-
 // audio 저장하기
-router.post('/upload', controller.parseForm, preprocess, upload.single('audio'), controller.uploadFile);
+router.post('/upload/preprocess', controller.parseForm, controller.preprocess, controller.uploadFileAfterPreprocesssing);
+router.post('/upload', controller.parseForm, controller.uploadFile);
  
 // audio 삭제하기
 router.delete('/:filename', controller.deleteFile);
