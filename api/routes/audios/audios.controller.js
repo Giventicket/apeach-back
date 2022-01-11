c11692d44f146c6871690d3ad6807deaae447015
root <root@apeach-back.c.pathfinder-101.internal>
2022-01-11 01:37:17 +0000

    [replace] googleStorageMulter -> googleCloudStorage

--- api/routes/audios/audios.controller.js
+++ api/routes/audios/audios.controller.js
@@ -1,13 +1,30 @@
+const formidable = require('formidable');
+const form = formidable({ multiples: true });
+
+const parseForm = (req, res, next) => {
+    form.parse(req, (err, fields, files) => {
+        try {
+            req.files = files;
+            console.log(files);
+            next();
+        } catch (err) {
+            next(err);
+        }
+    });
+};
+
 const uploadFile = async (req, res, next) => {
     try {
-        if(req.file === undefined){
-            const err = new Error("upload failed [no audio file]");
-            err.status = 400;
-            throw err;
-        }
+        const gcStorage = req.gcStorage;
+        const result = await gcStorage.bucket('apeach-bucket').upload(req.files.audio.filepath, {
+            destination: req.files.audio.newFilename,
+            metadata: {
+              contentType: req.files.audio.mimetype,
+            },
+        });
         res.status(200).json({
             message: "upload success [upload audio on google bucket]",
-            data: req.file
+            data: result[0].metadata
         });
     } catch (err) {
         next(err);
@@ -27,4 +44,4 @@ const deleteFile = async (req, res, next) => {
     }
 }
 
-module.exports = {uploadFile, deleteFile};
+module.exports = {uploadFile, deleteFile, parseForm};
--- api/routes/audios/index.js
+++ api/routes/audios/index.js
@@ -1,20 +1,31 @@
 const express = require('express');
-const multer = require('multer');
-const multerGoogleStorage = require('multer-google-storage');
 const controller = require('./audios.controller');
 
 const router = express.Router();
 
-const upload = multer({
-    storage: multerGoogleStorage.storageEngine({
-        bucket: process.env.BUCKET_NAME,
-        projectId: process.env.PROJECT_ID,
-        keyFilename: process.env.KEY_FILENAME
-    })
-});
+const preprocess = async (req, res, next) => {
+    try {
+        //ffmpeg -i {input} -ar 16000 -ac 1 -sample_fmt s16 {output}
+        let process = new ffmpeg(req.files.audio.filepath);
+        process.then((audio) => {
+            ffmpeg('/path/to/file.avi').inputOptions([
+            '-option1',
+            '-option2 param2',
+            '-option3',
+            '-option4 param4'
+            ]);
+        }, function (err) {
+            console.log('Error: ' + err);
+        });
+        next();
+    } catch (err) {
+        console.log(err);
+        next(err);
+    }
+}
 
 // audio 저장하기
-router.post('/upload', upload.single('audio'), controller.uploadFile);
+router.post('/upload', controller.parseForm, preprocess, upload.single('audio'), controller.uploadFile);
  
 // audio 삭제하기
 router.delete('/:filename', controller.deleteFile);
