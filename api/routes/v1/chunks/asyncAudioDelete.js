1cdac65f04b1fa11c2d9485058813ceac316cccc
root <root@apeach-back.c.pathfinder-101.internal>
2022-01-12 02:31:26 +0000

    [fix] google bucket deletion sync

--- api/routes/v1/chunks/asyncAudioDelete.js
+++ api/routes/v1/chunks/asyncAudioDelete.js
@@ -2,10 +2,11 @@ const url = require('url');
 
 const asyncAudioDelete = async (gcStorage, audio, logger) => {
     try {
-        const parsedAudio = url.parse(audio).path.split("/");
-        await gcStorage.bucket('apeach-bucket').file(parsedAudio[1]).delete();                     
-        logger.info(`[From Google Bucket] ${ parsedAudio[1] } is deleted on google bucket!, `)
-    } catch(err) {err.code || 
+        const parsedAudio = url.parse(audio).pathname.split('/');
+        await gcStorage.bucket(process.env.BUCKET_NAME).file(parsedAudio[7]).delete();                     
+        logger.info(`[From Google Bucket] ${ audio } is deleted!, `);
+    } catch(err) {
+        logger.info(`[From Google Bucket] ${ audio } is not deleted!, `);
         logger.error(`status: ${(err.status || err.code ||  500)}, message: ${err}`);
     };
 }
