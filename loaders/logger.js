7e57374bbfd3e950273e7aebc5286c1b9f656a74
Giventicket <jpseo99@gmail.com>
2022-01-17 13:55:14 +0900

    [fix] do not operate on production

--- loaders/logger.js
+++ loaders/logger.js
@@ -46,12 +46,14 @@ const logger = winston.createLogger({
   ],
 });
 
-logger.add(new winston.transports.Console({
-  format: winston.format.combine(
-    winston.format.colorize(),  // 색깔 넣어서 출력
-    winston.format.simple(),  // `${info.level}: ${info.message} JSON.stringify({ ...rest })` 포맷으로 출력
-  )
-}));
+if(process.env.NODE_ENV !== 'production'){
+  logger.add(new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.colorize(),  // 색깔 넣어서 출력
+      winston.format.simple(),  // `${info.level}: ${info.message} JSON.stringify({ ...rest })` 포맷으로 출력
+    )
+  }));
+}
 
 const stream = {
   write: message => {
