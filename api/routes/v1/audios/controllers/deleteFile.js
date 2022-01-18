fc9f0d98141aed00dcb0a5c0d7796328e06b6a6c
Giventicket <jpseo99@gmail.com>
2022-01-19 01:40:54 +0900

    [fix] decodeURIComponent

--- api/routes/v1/audios/controllers/deleteFile.js
+++ api/routes/v1/audios/controllers/deleteFile.js
@@ -2,7 +2,7 @@ const asyncErrorWrapper = require('../../public/asyncErrorWrapper.js');
 
 const deleteFile = asyncErrorWrapper((req, res, next) => {
     const gcStorage = req.gcStorage;
-    return gcStorage.bucket(process.env.BUCKET_NAME).file(req.params.filename).delete().then(() => {
+    return gcStorage.bucket(process.env.BUCKET_NAME).file(decodeURIComponent(req.params.filename)).delete().then(() => {
       res.status(200).json({
         message: `Delete success[${req.params.filename} from google bucket]`,
         data: { }
--- api/routes/v1/public/syncAudioDelete.js
+++ api/routes/v1/public/syncAudioDelete.js
@@ -4,7 +4,7 @@ const syncAudioDelete = (gcStorage, audio="", logger) => {
     if(audio == null)
       return;  
     const parsedAudio = url.parse(audio).pathname.split("/");
-    return gcStorage.bucket(process.env.BUCKET_NAME).file(parsedAudio[7]).delete().then(() => {
+    return gcStorage.bucket(process.env.BUCKET_NAME).file(decodeURIComponent(parsedAudio[7])).delete().then(() => {
       logger.info(`[From Google Bucket] ${ audio } is deleted!, `);
     }).catch(err => {
       logger.error(`[From Google Bucket] ${ audio } is not deleted!, `);
