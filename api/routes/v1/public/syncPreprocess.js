eec87c4cd2a9eb26448248688fcc40bbffb9b7d5
Giventicket <jpseo99@gmail.com>
2022-01-20 10:48:55 +0900

    [fix] unlink file if preprocessing fails

--- api/routes/v1/public/syncPreprocess.js
+++ api/routes/v1/public/syncPreprocess.js
@@ -1,4 +1,5 @@
 const ffmpeg = require('fluent-ffmpeg');
+const fs = require('fs').promises;
 
 const syncPreprocess = filepath => {
     //ffmpeg -i {input} -ar 16000 -ac 1 -sample_fmt s16 {output}
@@ -11,6 +12,13 @@ const syncPreprocess = filepath => {
                 resolve();
             })
             .on('error', err => {
+                fs.unlink(filepath).catch(err => {
+                    req.logger.error(
+                        `status: ${
+                            err.status || err.code || 500
+                        }, message: ${err}\n`,
+                    );
+                });
                 return reject(new Error(err));
             })
             .run();
