2700067046760fb55c6e0a8e5fbc7a831fe46640
Giventicket <jpseo99@gmail.com>
2022-01-26 22:23:50 +0900

    [fix] delete a file if it does not satisfy validation

--- api/routes/v1/audios/controllers/checkfile.js
+++ api/routes/v1/audios/controllers/checkfile.js
@@ -1,22 +1,27 @@
 const asyncErrorWrapper = require('../../../../../utils/asyncErrorWrapper.js');
 const asyncAudioValidate = require('../../../../../utils/asyncAudioValidate.js');
+const asyncFileDelete = require('../../../../../utils/asyncFileDelete.js');
 
 const checkFile = asyncErrorWrapper(async (req, res, next) => {
-    const metadata = await asyncAudioValidate(req.files.audio.filepath);
+    const filepath = req.files.audio.filepath;
+    const metadata = await asyncAudioValidate(filepath);
 
     if (metadata == null) {
         const err = new Error('validation failed[no metadata!]');
         err.status = 452;
+        asyncFileDelete(filepath);
         throw err;
     } else if (metadata.streams[0].codec_type !== 'audio') {
         const err = new Error('validation failed[not audio file!]');
         err.status = 453;
+        asyncFileDelete(filepath);
         throw err;
     } else if (metadata.streams[0].duration > 600) {
         const err = new Error(
             'validation failed[too long, over than 10 minutes!]',
         );
         err.status = 454;
+        asyncFileDelete(filepath);
         throw err;
     }
     req.duration = metadata.streams[0].duration;
