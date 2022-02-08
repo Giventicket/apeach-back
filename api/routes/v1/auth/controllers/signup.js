2429ccc1262b011831afe86c36edef9f671a3a78
Giventicket <jpseo99@gmail.com>
2022-02-08 12:14:09 +0900

    [fix] delete qualified

--- api/routes/v1/auth/controllers/signup.js
+++ api/routes/v1/auth/controllers/signup.js
@@ -2,7 +2,7 @@ const User = require('../../../../../models/v1/user/index');
 const asyncErrorWrapper = require('../../../../../utils/asyncErrorWrapper.js');
 
 const signup = asyncErrorWrapper(async (req, res, next) => {
-    const { name, password, qualified } = req.body;
+    const { name, password } = req.body;
     const user = await User.findOne({ name });
     if (user) {
         const err = new Error(`Duplicate name ${name}`);
