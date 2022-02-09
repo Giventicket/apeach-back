0c6d949bb0820bc0e99f71b0fdbe191a44301ab0
Giventicket <jpseo99@gmail.com>
2022-02-09 11:26:42 +0900

    [fix] add cookie secure option

--- loaders/express.js
+++ loaders/express.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
-
+const expressSession = require('express-session');
 const logger = require('../utils/logger');
 
 const indexRouter = require('../api/routes/index');
@@ -19,6 +19,18 @@ const combined =
 
 module.exports = app => {
     app.set('port', process.env.PORT || 80);
+    app.use(
+        expressSession({
+            resave: false,
+            saveUninitialized: false,
+            secret: process.env.COOKIE_SECRET,
+            cookie: {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'dev' ? false : true,
+                sameSite: process.env.NODE_ENV === 'dev' ? false : 'none',
+            },
+        }),
+    );
     app.use(cookieParser());
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
