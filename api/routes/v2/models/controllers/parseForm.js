8e99ef91a187c798289255aa831d8e92e2081ff1
Giventicket <jpseo99@gmail.com>
2022-02-15 13:34:13 +0900

    [feat] add headers

--- api/routes/v2/models/controllers/parseForm.js
+++ api/routes/v2/models/controllers/parseForm.js
@@ -1,7 +1,9 @@
 const asyncParseForm = require('../../../../../utils/asyncParseForm.js');
 const asyncErrorWrapper = require('../../../../../utils/asyncErrorWrapper.js');
+const logger = require('../../../../../utils/logger');
 
 const parseForm = asyncErrorWrapper(async (req, res, next) => {
+    logger.info(`look at me chad! ${req.headers}`);
     const files = await asyncParseForm(req);
     if (Object.keys(files).length === 0) {
         const err = new Error('upload failed [no model file]');
